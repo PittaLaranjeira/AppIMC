ca703512763a61ce4059ab380cdd4dd82a08ffa8
pittalaranjeira <vitoria.pitta01@fatec.sp.gov.br>
2023-02-20 23:52:11 -0300

    inserção cálculo IMC

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,29 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
 
+  tximc;
+  situacao;
+
+  calcimc(formu) {
+    const imcpeso = formu.value.imcpeso;
+    const imcaltura = formu.value.imcaltura;
+
+    this.tximc = (parseFloat(imcpeso) / (parseFloat(imcaltura) * parseFloat(imcaltura)));
+
+    if (this.tximc < 18.5) {
+      this.situacao = "Abaixo do Peso";
+    } else if (18.5 <= this.tximc && this.tximc <= 24.9) {
+      this.situacao = "Peso Ideal";
+    } else if (25 <= this.tximc && this.tximc <= 29.9) {
+      this.situacao = "Excesso de peso";
+    } else if (30 <= this.tximc && this.tximc <= 34.9) {
+      this.situacao = "Obesidade I";
+    } else if (35 <= this.tximc && this.tximc <= 39.9){
+      this.situacao = "Obesidade II";
+    } else if (this.tximc >= 40) {
+      this.situacao = "Obesidade III";
+    }
+  }
 }
